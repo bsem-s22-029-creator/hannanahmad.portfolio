4c6a4950f585b1fd8914f20a9b55041a55ec30e3
HananAhmad772 <hananshehbaz53@gmail.com>
2026-06-05 12:08:44 +0500

    update the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // Use '/' if you have a custom domain, otherwise use '/repo-name/'
+  base: '/',
+  build: {
+    outDir: 'dist',
+  }
 })
